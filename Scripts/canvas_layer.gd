c184f71bbf3c3d0044f61d8cf742a92a1532d73a
Codrin Ilie <codrin.ilie10@gmail.com>
2024-12-14 21:47:47 +0100

    New shader effect
    - Added the shiny effect to a color rect's material and made into a scene of its own
    - Added logic for instantiating the color rect when a ball hits a tile and then freeing it from the memory after its animation is done playing
    - Set the material's resource "Local to scene" to true to allow the effect to play on each individual instance separately

---
+++ Scripts/canvas_layer.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+class_name MainCanvasLayer
+
+const SHINY = preload("res://Scenes/Effects/shiny.tscn")
+
+func _ready() -> void:
+	Events.ball_hit_tile.connect(spawn_shiny)
+	
+func spawn_shiny() -> void:
+	var new_shiny_effect = SHINY.instantiate()
+	add_child(new_shiny_effect)
+	var shiny_animation_player = new_shiny_effect.get_child(0)
+	await shiny_animation_player.animation_finished
+	new_shiny_effect.queue_free()
